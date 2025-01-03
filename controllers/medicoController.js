37263b5a4a9f0a53de8dd7c854f579b2ddecf4d4
thomas semino <seminothomas8196@gmail.com>
2025-01-03 01:46:06 -0300

    fixed problems

--- controllers/medicoController.js
+++ controllers/medicoController.js
@@ -105,7 +105,7 @@ async function actualizarMedico(req, res) {
     try {
       const { id } = req.params;
       const medico = await MedicoLogic.actualizarMedico(id, req.body);
-      res.status(200).json(empresa);
+      res.status(200).json(medico);
     } catch (error) {
       res.status(500).json({ error: 'Error al actualizar al medico', detalle: error.message });
     }
