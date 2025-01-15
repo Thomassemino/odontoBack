8381f9b9c2a6e3123835b19589905a1d3ff70055
thomas semino <seminothomas8196@gmail.com>
2025-01-15 12:36:43 -0300

    idea de nefi

--- server.js
+++ server.js
@@ -7,20 +7,12 @@ const appRoutes = require("./routes");
 const port = process.env.PORT || 5000;
 // Configuración de CORS
 app.use(cors({
-  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: '*',
-  credentials: true,
+
 }));
 
-// Middleware para manejar OPTIONS manualmente
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200); // Responde con 200 OK
-});
+
 // Middleware para JSON y URL encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
