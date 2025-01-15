b2b5c07bd006200e5b8373448b256f1f481c2517
thomas semino <seminothomas8196@gmail.com>
2025-01-15 13:00:54 -0300

    gemini

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+//const cors = require("cors");
 const mongooseConnection = require("./helpers/mongoose-connection");
 const appRoutes = require("./routes");
 
 const port = process.env.PORT || 5000;
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.sendStatus(200);
-});
+
 // Configuración de CORS
-app.use(cors());
+app.use((req, res, next) => { res.setHeader('Access-Control-Allow-Origin', 'https://front-odonto-two.vercel.app'); res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); next(); });app.use(cors());
 
 
 // Middleware para JSON y URL encoded
@@ -25,6 +20,7 @@ mongooseConnection();
 
 // Rutas
 app.use("/api", appRoutes);
+app.get('/api/login/login', (req, res) => { res.json({ message: 'CORS configurado correctamente' }); });
 
 // Manejo de rutas no encontradas
 app.use((_, res) => {
