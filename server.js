b07c0409f94cc3873ef49fad322e0a1babbcb51b
thomas semino <seminothomas8196@gmail.com>
2025-01-15 11:57:48 -0300

    desactivacion de cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+//const cors = require("cors");
 const mongooseConnection = require("./helpers/mongoose-connection");
 const appRoutes = require("./routes");
 
 const port = process.env.PORT || 5000;
 
 // Configuración de CORS específica para tu aplicación
-app.use(cors());
+//app.use(cors());
 
 // Middleware para JSON y URL encoded
 app.use(express.json());
