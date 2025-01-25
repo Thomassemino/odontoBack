de9de155035e86a2757f403290686627f9940c20
Alejo <alejocstaf@proton.me>
2025-01-25 19:54:32 -0300

    odontograma

--- logic/odontogramaLogic.js
+++ logic/odontogramaLogic.js
@@ -1,8 +1,25 @@
 const Odontograma = require('../models/odontograma/odontogramaSchema');
+const Diente = require('../models/odontograma/dienteSchema');
+
 async function createOdontograma(data) {
-  return await Odontograma.create(data);
+  // Primero crear los dientes
+  const dientesCreados = await Promise.all(
+    data.dientes.map(diente => Diente.create(diente))
+  );
+
+  // Crear el odontograma con las referencias a los dientes
+  const odontogramaData = {
+    idPaciente: data.idPaciente,
+    dientes: dientesCreados.map(d => d._id)
+  };
+
+  return await Odontograma.create(odontogramaData);
 }
+
 async function getOdontogramaByPatientId(patientId) {
-  return await Odontograma.find({idPaciente: patientId}).populate('dientes');  
+  return await Odontograma.find({ idPaciente: patientId })
+    .populate('dientes')
+    .sort('-fecha');
 }
-module.exports = {createOdontograma, getOdontogramaByPatientId};
+
+module.exports = { createOdontograma, getOdontogramaByPatientId };
--- models/odontograma/dienteSchema.js
+++ models/odontograma/dienteSchema.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
-const dienteSchema = new mongoose.Schema({
-  numeroDiente: {type: String, required: true},
-  superficies: [{type: String, enum: ['Incisal', 'Oclusal', 'Mesial', 'Distal', 'Bucal', 'Labial', 'Palatina', 'Lingual', 'Cervical']}],
-  tratamiento: {type: String, enum: ['Restauración', 'Carilla de Porcelana', 'Sellador de Fisuras', 'Corona']},
+
+const esquemaDiente = new mongoose.Schema({
+  numeroDiente: { type: String, required: true },
+  superficie: { type: String, required: true },
+  tratamiento: { type: String, required: true },
+  notas: String
 });
-module.exports = mongoose.model('Diente', dienteSchema);
+
+module.exports = mongoose.model('Diente', esquemaDiente);
