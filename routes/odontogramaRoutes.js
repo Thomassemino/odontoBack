d2f1f7cc7315478d51822e01986c5c7c5a9a21e3
Alejo <alejocstaf@proton.me>
2025-01-26 12:35:10 -0300

    Odontograma deleted

--- routes/odontogramaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const odontogramaController = require('../controllers/odontogramaController'); 
-const router = express.Router();
-
-
-router.post('/odontograma', odontogramaController.create);
-router.get('/odontograma/patient/:patientId', odontogramaController.findByPatientId);
-
-module.exports = router;
