f3311b6f09b1484a4e1a7d01cce8b6042aefdf1b
Alejo <alejocstaf@proton.me>
2025-01-26 20:37:48 -0300

    Prestaciones

--- models/prestaciones/prestacionesSchema.js
+++ models/prestaciones/prestacionesSchema.js
@@ -1,11 +1,23 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 const prestacionesSchema = new mongoose.Schema({
   pacienteId: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'Paciente', 
-      required: [true, 'El ID del paciente es obligatorio']
-    },
-  precio: {type: Number, required: true, default: 0, message: 'El precio es obligatorio'},
-  descripcion: {type: String, required: true},
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Paciente', 
+    required: [true, 'El ID del paciente es obligatorio']
+  },
+  precio: {
+    type: Number, 
+    required: true, 
+    default: 0, 
+    message: 'El precio es obligatorio'
+  },
+  descripcion: {
+    type: String, 
+    required: true
+  },
+  pagado: {
+    type: Boolean,
+    default: false
+  }
 });
 module.exports = mongoose.model('Prestaciones', prestacionesSchema);
